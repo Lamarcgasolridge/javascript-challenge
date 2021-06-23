f1952523287048de8189dde5eec1d7b276a00cec
Lamarcgasolridge <80485596+Lamarcgasolridge@users.noreply.github.com>
2021-06-23 13:59:35 -0700

    Complete JS code

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+let tbody = d3.select("tbody");
+
+function buildTable(data){
+
+    tbody.html("");
+
+    data.forEach((dataRow) => {
+
+        let row = tbody.append("tr");
+               
+        Object.values(dataRow).forEach((val) => {
+           let cell = row.append("td");
+           cell.text(val);
+       });
+    })
+}
+
+// create function to filter by date input
+function handleClick() {
+    let dateInput = d3.select("#datetime").property("value")
+    let filteredData = data.filter((a) => a.datetime === dateInput)
+
+buildTable(filteredData)
+}
+
+// pass id for button and function to click
+d3.select("#filter-btn").on("click", handleClick)
+
+
+buildTable(tableData);
